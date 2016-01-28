18ea66beec1010c386a60e2ddfa5de988436b92a
Vadym Abramchuk <abramm@gmail.com>
2016-01-28 15:07:48 +0200

    Add MySQL, add UIDs mapping

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -3,6 +3,14 @@ set -e
 
 cat /usr/local/share/logo.txt
 
+if [ -f /tmp/dummyfile ]; then
+	echo Mapping UIDs...
+	OLDUID=`id -u www-data`
+	NEWUID=`stat -c '%u' /tmp/dummyfile`
+	usermod -u $NEWUID www-data
+	find /var/www -user $OLDUID -exec chown -h $NEWUID {} \;
+fi
+
 chown -R www-data:www-data /var/www/html
 if [ -z "$(ls -A "/var/www/html")" ]; then
 	echo "Oh so you don't have a Drupal. No worries, we'll make one for you."
